a333bb41fa6012f716dd2dca9f575374748f89ba
Bojan Bilić <excaliboor@users.noreply.github.com>
2016-05-30 23:06:13 +0200

    spacing

--- FlagGen.js
+++ FlagGen.js
@@ -1,31 +1,31 @@
 function FlagGenerator ()
 {
-	this.identifiers = {};
+    this.identifiers = {};
 
-	this.next_flag_for_id = function (identifier) {
+    this.next_flag_for_id = function (identifier) {
     // examples:
 	// FG.next_flag_for_id( “AUDIO_OUTPUTS” )
 	// FG.next_flag_for_id( “VIDEO_INPUT_PORTS” )
 	// FG.next_flag_for_id( “ACCESS_FLAGS” )
         if (typeof (identifier) == "object")
-		{
-			console.error( "[FlagGenerator] Identifier must not be an object" );
-			return NaN;
-		}
+        {
+            console.error( "[FlagGenerator] Identifier must not be an object" );
+            return NaN;
+        }
 
-		if (typeof (this.identifiers[identifier]) == "undefined")
-			this.identifiers[identifier] = 0; // create
+        if (typeof (this.identifiers[identifier]) == "undefined")
+            this.identifiers[identifier] = 0; // create
 
-		return 0x1<<this.identifiers[identifier]++;
-	};
+        return 0x1<<this.identifiers[identifier]++;
+    };
 
-	this.all = function (identifier) {
+    this.all = function (identifier) {
     // examples:
 	// FG.all( “AUDIO_OUTPUTS” )
 	// FG.all( “VIDEO_INPUT_PORTS” )
 	// FG.all( “ACCESS_FLAGS” )
-		return (0x1<<this.identifiers[identifier])-1;
-	};
+        return (0x1<<this.identifiers[identifier])-1;
+    };
 };
 
 var FG = new FlagGenerator();
